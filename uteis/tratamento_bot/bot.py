46d1605af949212fd88ca2a51ca47e030e5a90ea
the_jota <jeremiassantana50@gmail.com>
2026-09-05 03:14:01 -0300

    Aprimoramento da logica e aplicando poo

--- uteis/tratamento_bot/bot.py
+++ uteis/tratamento_bot/bot.py
@@ -1,6 +1,5 @@
-import time
-
 from botcity.web import Browser, By, WebBot
+
 from uteis.tratamento_log.logger import loggin
 
 
@@ -33,6 +32,8 @@ class BotWeb:
         except Exception as erro:
             self.logger.exception(f"Aconteceu um erro inesperado ao tentar abrir o navegador!!: {erro}")
             raise
+
+
 
 class ClickCoockie(BotWeb):
     def __init__(self):
@@ -69,15 +70,17 @@ class ClickCoockie(BotWeb):
 
             self.bot.wait_for_element_visibility(idioma)
             idioma.click()
+
             self.logger.info('Idioma PT-BR selecionado!!')
             return
 
         except ConnectionError:
                     self.logger.error("Houve um erro de conexão na abertura do site")
-        
+
         except Exception as erro:
             self.logger.exception(f"Houve um erro inesperado ao tentar abrir o site: {erro}")
             raise
+    
 
 
     def cliques(self):
@@ -86,19 +89,17 @@ class ClickCoockie(BotWeb):
 
             big_cockie = self.bot.find_element(
                 selector="bigCookie",
-                by=By.ID
+                by=By.ID,
+                ensure_clickable=True
             )
 
             if not big_cockie:
                 self.logger.error("Não foi possivel achar o elemento")
-                return
+                
 
             self.bot.wait_for_element_visibility(big_cockie)
-            self.logger.info("O robô começou a clicar")
-
-            while True:
-                big_cockie.click()
-                time.sleep(0.2)
+            big_cockie.click()
+            return True
 
         except ConnectionError:
             self.logger.exception("Houve um erro de internet. O robô parou")
@@ -106,19 +107,74 @@ class ClickCoockie(BotWeb):
 
         except Exception as erro:
             self.logger.exception(f"Houve um erro inesperado na clicagem do big cockie: {erro}")
-            raise
+            return False
 
 
+    def melhorias(self):
+        try:
+            upgrade0 = self.bot.find_element(
+                selector="upgrade0",
+                by=By.ID,
+                waiting_time=99999999999999,
+                ensure_clickable=True
+            )
 
+            upgrade0.click()                                
+            # self.logger.info(f"\033[31mUpgrade\033[0m: {upgrade0.text} \033[31mcomprada\033[0m")
+            return True
+                
+        except Exception as erro:
+            self.logger.exception(f"Houve um erro disconhecido ao tentar comprar upgrades: {erro}")
+            return False
+
+
+    def contrucoes(self):
+        try:
+            contrucao = self.bot.find_elements(
+                selector=".product.unlocked.enabled",
+                by=By.CSS_SELECTOR,
+                waiting_time=999999999999,
+                ensure_visible=True
+            )
+
+            for con in contrucao:
+                con.click()
+            return True
+
+
+        except Exception as erro:
+            self.logger.exception(f"Houve um erro ao tentar comprar contrução: {erro}")
+            return False
 
 
 site = BotWeb()
 jogo = ClickCoockie()
 
-jogo.config_navegador()
-jogo.abrir_navegador()
-jogo.abertura_site()
-jogo.selecao_idioma()
-jogo.cliques()
+if __name__ == "__main__":
+    try:
+        jogo.config_navegador()
+        jogo.abrir_navegador()
+        jogo.abertura_site()
+        jogo.selecao_idioma()
 
 
+        logge = loggin()
+        logge.info("O robô começou a clicar")
+
+        while True:
+            if not jogo.cliques():
+                continue
+
+            for _ in range(50):
+                jogo.cliques()
+                jogo.cliques()
+
+            if not jogo.contrucoes():
+                continue
+
+            if not jogo.melhorias():
+                continue
+
+    except ConnectionError:
+        logge.exception("A conexão caiu no meio do processo")
+        raise
